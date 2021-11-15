2cb80927ca345b43b3c6f9d27046c92bdfddc386
Alessandro Daducci <alessandro.daducci@univr.it>
2021-11-15 17:36:47 +0100

    Hide subfunctions from imports

--- dicelib/__init__.py
+++ dicelib/__init__.py
@@ -1,4 +1,6 @@
 __all__ = ['ui', 'clustering']
+from . import ui
+from . import clustering
 
-from pkg_resources import get_distribution
-__version__ = get_distribution('dicelib').version
+from pkg_resources import get_distribution as _get_distribution
+__version__ = _get_distribution('dicelib').version
