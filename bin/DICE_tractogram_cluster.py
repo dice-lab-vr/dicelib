352dd02f1fcf579cc71214a4a707545189392afe
Alessandro Daducci <alessandro.daducci@univr.it>
2021-11-18 16:20:30 +0100

    Multiple thresholds allowed

--- bin/DICE_tractogram_cluster.py
+++ bin/DICE_tractogram_cluster.py
@@ -9,7 +9,7 @@ def input_parser():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("input_tractogram", help="Input tractogram")
     parser.add_argument("output_tractogram", help="Output tractogram")
-    parser.add_argument("threshold", type=float, help="Threshold [in mm]")
+    parser.add_argument("--thresholds", "-t", type=float, nargs='+', metavar="THR", help="Threshold(s) [in mm]")
     parser.add_argument("--reference", "-r", action="store", help="Space attributes used as reference for the input tractogram")
     parser.add_argument("--n_pts", type=int, default=12, help="Number of points for the resampling of a streamline")
     parser.add_argument("--replace_centroids", action="store_true", help="Replace centroids with closer streamline in a cluster")
@@ -35,6 +35,8 @@ def main():
     parser = input_parser()
     options = parser.parse_args()
     
+    print( options.thresholds )
+
     # check input
     if not os.path.isfile(options.input_tractogram):
         parser.error( f'File "{options.input_tractogram}" not found' )
@@ -50,7 +52,7 @@ def main():
         options.input_tractogram,
         options.reference,
         options.output_tractogram,
-        [options.threshold],
+        options.thresholds,
         n_pts=options.n_pts,
         replace_centroids=options.replace_centroids,
         random=options.random,
