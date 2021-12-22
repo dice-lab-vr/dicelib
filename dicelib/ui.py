e6ea2ead59b3f5c0b317ba06076fa45c054699cd
Alessandro Daducci <alessandro.daducci@univr.it>
2021-12-22 13:01:15 +0100

    Changed to fMagenta

--- dicelib/ui.py
+++ dicelib/ui.py
@@ -122,7 +122,7 @@ class ColoredArgParser( _ArgumentParser ):
 	"""Modification of 'argparse.ArgumentParser' to allow colored output.
 	"""
 	class _ColoredFormatter( _ArgumentDefaultsHelpFormatter ):
-		COLOR = fBlue
+		COLOR = fMagenta
 
 		def start_section(self, heading):
 			super().start_section( Underline+heading.capitalize()+Reset )
