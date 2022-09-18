bccdf1cbac771af252b27aa207f42bd1bdcb7849
AngeloVella1995 <angelo.vella@studenti.univr.it>
2022-09-18 22:07:11 +0200

    secondcommit

---
+++ bin/dice_tractogram_sample.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from dicelib.ui import ColoredArgParser
+#from dicelib.tractogram import sample 
+
+# parse the input parameters
+
+# call actual function
