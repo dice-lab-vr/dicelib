1a93c419e04eed6cbe2cf60b2ab3adbe5b4b384c
AngeloVella1995 <angelo.vella@studenti.univr.it>
2022-09-21 13:08:15 +0200

    3commit

--- dicelib/streamline.pyx
+++ dicelib/streamline.pyx
@@ -35,7 +35,7 @@ cpdef length( float [:,:] streamline, int n=0 ):
 
 
 def sampling(float [:,:] streamline, double [:,:,:] img, int npoints = 0):
-    v = np.empty([1,3], dtype= float)
+    v = np.empty([3,], dtype= float)
     cdef float value 
     cdef size_t ii , yy 
     for ii in range(npoints):    
