3bd449212003c8298f443d78a43e61d89d0bffdc
nightwnvol <clorindo.notte@univr.it>
2024-02-20 13:31:34 +0100

    refactor: add tractogram_tsf to scripts

--- dicelib/scripts/dice_tractogram.py
+++ dicelib/scripts/dice_tractogram.py
@@ -8,11 +8,62 @@ from time import time
 from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 from dicelib.lazytractogram import LazyTractogram
+from dicelib.tsf import Tsf
 
 def compute_chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
+def create_color_scalar_file(streamline, num_streamlines):
+        """
+        Create a scalar file for each streamline in order to color them.
+        Parameters
+        ----------
+        streamlines: list
+            List of streamlines.
+        Returns
+        -------
+        scalar_file: str
+            Path to scalar file.
+        """
+        scalar_list = list()
+        n_pts_list = list()
+        for i in range(num_streamlines):
+            # pt_list = list()
+            streamline.read_streamline()
+            n_pts_list.append(streamline.n_pts)
+            for j in range(streamline.n_pts):
+                scalar_list.extend([float(j)])
+            # scalar_list.append(pt_list)
+        return np.array(scalar_list, dtype=np.float32), np.array(n_pts_list, dtype=np.int32)
+
+def color_by_scalar_file(streamline, values, num_streamlines):
+    """
+    Color streamlines based on sections.
+    Parameters
+    ----------
+    streamlines: array
+        Array of streamlines.
+    values: list
+        List of scalars.
+    Returns
+    -------
+    array
+        Array mapping scalar values to each vertex of each streamline.
+    array
+        Array containing the number of points of each input streamline.
+    """
+    scalar_list = []
+    n_pts_list = []
+    for i in range(num_streamlines):
+        streamline.read_streamline()
+        n_pts_list.append(streamline.n_pts)
+        streamline_points = np.arange(streamline.n_pts)
+        resample = np.linspace(0, streamline.n_pts, len(values), endpoint=True, dtype=np.int32)
+        streamline_points = np.interp(streamline_points, resample, values)
+        scalar_list.extend(streamline_points)
+    return np.array(scalar_list, dtype=np.float32), np.array(n_pts_list, dtype=np.int32)
 
 def tractogram_filter():
     # parse the input parameters
@@ -605,3 +656,48 @@ def tractogram_split():
         options.verbose,
         options.force
     )
+
+def tractogram_tsf():
+    # parse the input parameters
+    parser = ColoredArgParser(description='Create a tsf file for each streamline in order to color them.')
+    args = [
+        [['input_tractogram'], {'type': str, 'help': 'Input tractogram'}],
+        [['output_tsf'], {'type': str, 'help': 'Output tsf filename'}],
+        [['--orientation'], {'action': 'store_true', 'default': False, 'help': 'Color based on orientation'}],
+        [['--file'], {'type': str, 'help': 'Color based on given file'}],
+        [['--force', '-f'], {'action': 'store_true', 'help': 'Force overwriting of the output'}]
+    ]
+    for arg in args:
+        parser.add_argument(*arg[0], **arg[1])
+    options = parser.parse_args()
+
+    # check if path to input and output files are valid
+    if not isfile(options.input_tractogram):
+        ERROR(f"Input tractogram file not found: {options.input_tractogram}")
+    if isfile(options.output_tsf) and not options.force:
+        ERROR(
+            f"Output tsf file already exists: {options.output_tsf}, "
+            "use -f to overwrite")
+    if not options.orientation and not options.file:
+        ERROR("Please specify a color option")
+    if options.file:
+        if not isfile(options.file):
+            ERROR(f"Input file not found: {options.file}")
+
+    streamline = LazyTractogram(options.input_tractogram, mode='r')
+    num_streamlines = streamline.header['count']
+
+    if options.orientation:
+        scalar_arr, n_pts_list = create_color_scalar_file(streamline, int(num_streamlines))
+    elif options.file:
+        values = np.loadtxt(options.file)
+        scalar_arr, n_pts_list = color_by_scalar_file(streamline, values, int(num_streamlines))
+    else:
+        raise ValueError("Please specify a color option")
+
+    # check if output file exists
+    if isfile(options.output_tsf) and not options.force:
+        raise IOError("Output file already exists. Use -f to overwrite.")
+
+    tsf = Tsf(options.output_tsf, 'w', header=streamline.header)
+    tsf.write_scalar(scalar_arr, n_pts_list)
