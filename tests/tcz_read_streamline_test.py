7bf3931f45a2bc79b9e1928a06c4d3f6e41ac1aa
Giorgio Costantino <ggh2e3@proton.me>
2024-05-31 18:23:02 +0200

    fixed file and test

--- tests/tcz_read_streamline_test.py
+++ tests/tcz_read_streamline_test.py
@@ -12,73 +12,78 @@ def test_read_fake_streamline_successfully():
 
 
 def test_read_streamline_with_spline_with_little_epsilon_will_return_all_points():
-    # TCZ having a streamline with all its points
+
     tcz = Tcz('tests/dicelib/mock/demo_fibers_to_smooth.tcz', mode='r')
     n_points, streamline = tcz.read_streamline()
     assert n_points == 100
 
-    assert streamline[0][0] == pytest.approx(49.40, abs=0.01)
-    assert streamline[0][1] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[0][2] == pytest.approx(22.95, abs=0.01)
+    # check only some points
+    assert streamline[0][0] == 49.40625
+    assert streamline[0][1] == 22.0
+    assert streamline[0][2] == 24.5
 
-    assert streamline[1][0] == pytest.approx(24.5, abs=0.01)
-    assert streamline[1][1] == pytest.approx(48.9, abs=0.01)
-    assert streamline[1][2] == pytest.approx(-1.98, abs=0.01)
+    assert streamline[1][0] == 48.90625
+    assert streamline[1][1] == 22.046875
+    assert streamline[1][2] == 24.5
 
-    assert streamline[2][0] == pytest.approx(22.95, abs=0.01)
-    assert streamline[2][1] == pytest.approx(24.5, abs=0.01)
-    assert streamline[2][2] == pytest.approx(48.31, abs=0.01)
+    assert streamline[2][0] == 48.40625
+    assert streamline[2][1] == 22.09375
+    assert streamline[2][2] == 24.5
 
-    assert streamline[3][0] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[3][1] == pytest.approx(22.95, abs=0.01)
-    assert streamline[3][2] == pytest.approx(24.5, abs=0.01)
+    assert streamline[3][0] == 47.90625
+    assert streamline[3][1] == 22.140625
+    assert streamline[3][2] == 24.5
 
-    assert streamline[4][0] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[4][1] == pytest.approx(45.9, abs=0.01)
-    assert streamline[4][2] == pytest.approx(-1.98, abs=0.01)
+    assert streamline[4][0] == 47.40625
+    assert streamline[4][1] == 22.171875
+    assert streamline[4][2] == 24.5
 
-    assert streamline[5][0] == pytest.approx(22.95, abs=0.01)
-    assert streamline[5][1] == pytest.approx(24.5, abs=0.01)
-    assert streamline[5][2] == pytest.approx(-1.98, abs=0.01)
+    assert streamline[5][0] == 46.90625
+    assert streamline[5][1] == 22.203125
+    assert streamline[5][2] == 24.5
 
-    assert streamline[6][0] == pytest.approx(45.9, abs=0.01)
-    assert streamline[6][1] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[6][2] == pytest.approx(22.95, abs=0.01)
+    assert streamline[6][0] == 46.40625
+    assert streamline[6][1] == 22.234375
+    assert streamline[6][2] == 24.5
 
-    assert streamline[7][0] == pytest.approx(24.5, abs=0.01)
-    assert streamline[7][1] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[7][2] == pytest.approx(45.9, abs=0.01)
+    assert streamline[7][0] == 45.90625
+    assert streamline[7][1] == 22.265625
+    assert streamline[7][2] == 24.5
 
-    assert streamline[8][0] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[8][1] == pytest.approx(22.95, abs=0.01)
-    assert streamline[8][2] == pytest.approx(24.5, abs=0.01)
+    assert streamline[8][0] == 45.375
+    assert streamline[8][1] == 22.28125
+    assert streamline[8][2] == 24.5
 
-    assert streamline[9][0] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[9][1] == pytest.approx(45.9, abs=0.01)
-    assert streamline[9][2] == pytest.approx(-1.98, abs=0.01)
+    assert streamline[9][0] == 44.875
+    assert streamline[9][1] == 22.296875
+    assert streamline[9][2] == 24.5
 
-    assert streamline[10][0] == pytest.approx(22.95, abs=0.01)
-    assert streamline[10][1] == pytest.approx(24.5, abs=0.01)
-    assert streamline[10][2] == pytest.approx(-1.98, abs=0.01)
+    assert streamline[10][0] == 44.375
+    assert streamline[10][1] == 22.3125
+    assert streamline[10][2] == 24.5
 
-    assert streamline[11][0] == pytest.approx(45.9, abs=0.01)
-    assert streamline[11][1] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[11][2] == pytest.approx(22.95, abs=0.01)
+    assert streamline[11][0] == 43.875
+    assert streamline[11][1] == 22.328125
+    assert streamline[11][2] == 24.5
 
-    assert streamline[12][0] == pytest.approx(24.5, abs=0.01)
-    assert streamline[12][1] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[12][2] == pytest.approx(45.9, abs=0.01)
+    assert streamline[12][0] == 43.375
+    assert streamline[12][1] == 22.34375
+    assert streamline[12][2] == 24.5
 
-    assert streamline[13][0] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[13][1] == pytest.approx(22.95, abs=0.01)
-    assert streamline[13][2] == pytest.approx(24.5, abs=0.01)
+    assert streamline[13][0] == 42.875
+    assert streamline[13][1] == 22.359375
+    assert streamline[13][2] == 24.5
 
-    assert streamline[14][0] == pytest.approx(-1.98, abs=0.01)
-    assert streamline[14][1] == pytest.approx(45.9, abs=0.01)
-    assert streamline[14][2] == pytest.approx(-1.98, abs=0.01)
+    assert streamline[14][0] == 42.375
+    assert streamline[14][1] == 22.359375
+    assert streamline[14][2] == 24.5
 
-    assert streamline[15][0] == pytest.approx(22.95, abs=0.01)
-    assert streamline[15][1] == pytest.approx(24.5, abs=0.01)
-    assert streamline[15][2] == pytest.approx(-1.98, abs=0.01)
+    assert streamline[15][0] == 41.875
+    assert streamline[15][1] == 22.359375
+    assert streamline[15][2] == 24.5
+
+    assert streamline[99][0] == -0.39990234375
+    assert streamline[99][1] == 22.0
+    assert streamline[99][2] == 24.5
 
     tcz.close()
