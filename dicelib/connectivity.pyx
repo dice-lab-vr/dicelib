95d33929a2ca242b76722ee919d5421956f60c5f
Ilaria Gabusi <ilariagabusi@gmail.com>
2026-05-15 15:22:45 +0200

    Changed parameter names in build_connectome()

--- dicelib/connectivity.pyx
+++ dicelib/connectivity.pyx
@@ -542,31 +542,31 @@ def compute_connectome_blur(input_tractogram: str, output_connectome: str, weigh
 
 
 
-def build_connectome( input_assignments: str, output_connectome: str, input_weights: str=None, metric: str='sum', symmetric: bool=False, input_tractogram: str=None, input_nodes: str=None, atlas_dist: float=2.0, n_threads: int=None, verbose: int=3, force: bool=False, log_list=None ):
-    """Build the (weighted) connectome having the assignments or the tractogram and an atlas.
+def build_connectome( assignments_filename: str, out_connectome_filename: str, weights_filename: str=None, stat: str='sum', symmetric: bool=False, tractogram_filename: str=None, atlas_filename: str=None, distance: float=2.0, n_threads: int=None, verbose: int=3, force: bool=False, log_list=None ):
+    """Build the (weighted) connectome from the pre-computed streamline assignments or providing the tractogram and an atlas.
 
     Parameters
     ----------
-    input_assignments : str # to change in assignments_filename
+    assignments_filename : str
         Path to the file (.txt, .npy) containing the streamline assignments.
         Note: if it does not exist, the assignments file will be created from the given tractogram and atlas.
-    output_connectome : str # to change in out_connectome_filename
+    out_connectome_filename : str
         Path to the file (.csv, .npy) that will contain the resulting connectome.
-    input_weights : str # to change in weights_filename
+    weights_filename : str
         Path to the scalar file (.txt, .npy) containing the streamline weights used to compute the value of the connectome edges.
         If None, the value of the edges will be number of streamline connecting the corresponding regions.
-    metric : {'sum', 'mean', 'min', 'max'} default='sum' # to change in stat
+    stat : {'sum', 'mean', 'min', 'max'} default='sum'
         Summary statistic to compute the value of the connectome edges.
         Note: if no weights are provided, this parameter will be ignored.
     symmetric : boolean, default=False
         Make the output connectome symmetric.
-    input_tractogram : str # to change in tractogram_filename
+    tractogram_filename : str
         Path to the file (.tck) containing the streamlines to process.
         Required if the assignments file does not exist.
-    input_nodes : str # to change in atlas_filename
+    atlas_filename : str
         Path to the file (.nii, .nii.gz) containing the labels of the atlas (nodes of the connectome).
         Required if the assignments file does not exist.
-    atlas_dist : float, default=2.0 # to change in distance
+    distance : float, default=2.0
         Distance [in mm] to consider in the radial search when computing the assignments.
         Note: this parameter is used only if the assignments file does not exist and needs to be computed from the input tractogram and atlas.
     n_threads : int, deault=None
@@ -583,83 +583,83 @@ def build_connectome( input_assignments: str, output_connectome: str, input_weig
     t0 = time()
 
     files = [
-        File(name='connectome_out', type_='output', path=output_connectome, ext=['.csv', '.npy'])
+        File(name='out_connectome_filename', type_='output', path=out_connectome_filename, ext=['.csv', '.npy'])
     ]
-    if input_weights is not None:
-        files.append(File(name='weights_in', type_='input', path=input_weights, ext=['.txt', '.npy']))
-    if os.path.isfile(input_assignments):
-        files.append(File(name='assignments_in', type_='input', path=input_assignments, ext=['.txt', '.npy']))
+    if weights_filename is not None:
+        files.append(File(name='weights_filename', type_='input', path=weights_filename, ext=['.txt', '.npy']))
+    if os.path.isfile(assignments_filename):
+        files.append(File(name='assignments_filename', type_='input', path=assignments_filename, ext=['.txt', '.npy']))
     else:
         # check input tractogram and parcellation
-        if input_tractogram is None:
+        if tractogram_filename is None:
             logger.error(f'Tractogram file not provided. Required if the assignments does not exist.')
-        if input_nodes is None:
-            logger.error(f'Nodes file not provided. Required if the assignments does not exist.')
+        if atlas_filename is None:
+            logger.error(f'Atlas file not provided. Required if the assignments does not exist.')
         files.extend([
-            File(name='tractogram_in', type_='input', path=input_tractogram, ext=['.tck']),
-            File(name='nodes_in', type_='input', path=input_nodes, ext=['.nii', '.nii.gz'])
+            File(name='tractogram_filename', type_='input', path=tractogram_filename, ext=['.tck']),
+            File(name='atlas_filename', type_='input', path=atlas_filename, ext=['.nii', '.nii.gz'])
         ])
 
         # logger.info('No assignments file found. Computing assignments')
-        logger.subinfo(f'Input tractogram: \'{input_tractogram}\'', indent_char='*', indent_lvl=1)
-        logger.subinfo(f'Input parcellation: \'{input_nodes}\'', indent_char='*', indent_lvl=1)
+        logger.subinfo(f'Input tractogram: \'{tractogram_filename}\'', indent_char='*', indent_lvl=1)
+        logger.subinfo(f'Input atlas: \'{atlas_filename}\'', indent_char='*', indent_lvl=1)
 
         # compute assignments
         log_list2 = []
         ret_subinfo2 = logger.subinfo('Computing assignments', indent_lvl=1, indent_char='*', with_progress=verbose>2)
         with ProgressBar(disable=verbose < 3, hide_on_exit=True, subinfo=ret_subinfo2, log_list=log_list2) as pbar:
-            assign(input_tractogram, input_nodes, input_assignments, atlas_dist, verbose=1, n_threads=n_threads, log_list=log_list2)
+            assign(tractogram_filename, atlas_filename, assignments_filename, distance, verbose=1, n_threads=n_threads, log_list=log_list2)
         set_verbose('connectivity', verbose)
 
     check_params(files=files, force=force)
 
     # streamline assignments
-    input_assignments_ext = os.path.splitext(input_assignments)[1]
-    if input_assignments_ext=='.txt':
-        asgn = np.loadtxt( input_assignments ).astype(np.int32)
+    assignments_filename_ext = os.path.splitext(assignments_filename)[1]
+    if assignments_filename_ext=='.txt':
+        asgn = np.loadtxt( assignments_filename ).astype(np.int32)
     else:
-        asgn = np.load( input_assignments, allow_pickle=False ).astype(np.int32)
+        asgn = np.load( assignments_filename, allow_pickle=False ).astype(np.int32)
     n_streamlines = asgn.shape[0]
     asgn_sort = np.sort(asgn, axis=1) # shape = (n_streamlines, 2)
 
     # check if the assignments match with the number of streamlines in the tractogram
-    if input_tractogram is not None:
-        TCK_in = LazyTractogram( input_tractogram, mode='r' )
+    if tractogram_filename is not None:
+        TCK_in = LazyTractogram( tractogram_filename, mode='r' )
         n_str_tck = int( TCK_in.header['count'] )
         TCK_in.close()
         if n_streamlines != n_str_tck:
             logger.error(f'Number of streamlines in the tractogram ({n_str_tck}) is different from the number of streamline assignments ({n_streamlines})')
 
     # streamline weights
-    if input_weights is None:
+    if weights_filename is None:
         w = np.ones( n_streamlines, dtype=np.int32 )
     else:
-        input_weights_ext = os.path.splitext(input_weights)[1]
-        if input_weights_ext=='.txt':
-            w = np.loadtxt( input_weights ).astype(np.float64)
-        elif input_weights_ext=='.npy':
-            w = np.load( input_weights, allow_pickle=False ).astype(np.float64)
+        weights_filename_ext = os.path.splitext(weights_filename)[1]
+        if weights_filename_ext=='.txt':
+            w = np.loadtxt( weights_filename ).astype(np.float64)
+        elif weights_filename_ext=='.npy':
+            w = np.load( weights_filename, allow_pickle=False ).astype(np.float64)
         # check if #(weights)==n_streamlines
         if n_streamlines != w.size:
             logger.error(f'Number of weights ({w.size}) is different from the number of streamline assignments ({n_streamlines})')
 
-    # metric
-    if metric not in ['sum', 'mean', 'min', 'max']:
-        logger.error('Invalid type of metric for the edges. Options: sum, mean, min, max.')
-    if input_weights is None:
-        metric = 'sum' # to compute connectome NOS
+    # stat for edges
+    if stat not in ['sum', 'mean', 'min', 'max']:
+        logger.error('Invalid type of statistic for the edges. Available options: sum, mean, min, max.')
+    if weights_filename is None:
+        stat = 'sum' # to compute connectome NOS
 
 
-    logger.subinfo(f'Streamline assignments: "{input_assignments}"', indent_char='*', indent_lvl=1)
-    if input_weights is not None:
-        logger.subinfo(f'Chosen metric to weight the edges: {metric}', indent_char='*', indent_lvl=1)
-        logger.subinfo(f'Input weights: "{input_weights}"', indent_char='*', indent_lvl=1)
+    logger.subinfo(f'Streamline assignments: "{assignments_filename}"', indent_char='*', indent_lvl=1)
+    if weights_filename is not None:
+        logger.subinfo(f'Chosen statistic to weight the edges: {stat}', indent_char='*', indent_lvl=1)
+        logger.subinfo(f'Input weights: "{weights_filename}"', indent_char='*', indent_lvl=1)
     else:
         logger.subinfo('No weights provided, the connectome will contain the number of streamlines', indent_char='*', indent_lvl=1)
 
     # create connectome to fill
-    if input_nodes is not None:
-        gm_nii = nib.load(input_nodes)
+    if atlas_filename is not None:
+        gm_nii = nib.load(atlas_filename)
         gm = gm_nii.get_fdata()
         n_rois = np.max(gm).astype(np.int32)
         logger.subinfo(f'Number of regions: {n_rois}', indent_char='*', indent_lvl=1)
@@ -667,26 +667,26 @@ def build_connectome( input_assignments: str, output_connectome: str, input_weig
         n_rois = np.max(asgn).astype(np.int32)
         logger.subinfo(f'Number of regions: {n_rois}', indent_char='*', indent_lvl=1)
 
-    if metric == 'min':
+    if stat == 'min':
         conn = np.triu(np.full((n_rois, n_rois), 1000000000, dtype=np.float64))
-    elif metric == 'max':
+    elif stat == 'max':
         conn = np.triu(np.full((n_rois, n_rois), -1000000000, dtype=np.float64))
     else:
         conn = np.zeros((n_rois, n_rois), dtype=np.float64)
     conn_nos = np.zeros((n_rois, n_rois), dtype=np.float64)
     count_unconn = 0
 
-    logger.subinfo('Building connectome', indent_char='*', indent_lvl=1)
+    logger.subinfo('Computing connectome values', indent_char='*', indent_lvl=1)
     with ProgressBar( total=n_streamlines, disable=verbose < 3, hide_on_exit=True, subinfo=False) as pbar:
         for i in range( n_streamlines ):
             if asgn_sort[i][0] == 0 or asgn_sort[i][1] == 0:
                 count_unconn += 1
                 continue
 
-            if metric == 'min':
+            if stat == 'min':
                 if w[i] < conn[asgn_sort[i][0]-1, asgn_sort[i][1]-1]:
                     conn[asgn_sort[i][0]-1, asgn_sort[i][1]-1] = w[i]
-            elif metric == 'max':
+            elif stat == 'max':
                 if w[i] > conn[asgn_sort[i][0]-1, asgn_sort[i][1]-1]:
                     conn[asgn_sort[i][0]-1, asgn_sort[i][1]-1] = w[i]
             else: # sum or mean
@@ -697,26 +697,25 @@ def build_connectome( input_assignments: str, output_connectome: str, input_weig
         warning_msg = f'Number of non-connecting streamlines {count_unconn}'
         logger.warning(warning_msg) if log_list is None else log_list.append(warning_msg)
 
-    if metric == 'mean':
+    if stat == 'mean':
         conn[conn_nos>0] = conn[conn_nos>0]/conn_nos[conn_nos>0]
 
     conn[conn_nos==0] = 0
-    # np.save(output_connectome[:-4]+'NOS.npy', conn_nos, allow_pickle=False)
 
-    conn_out_ext = os.path.splitext(output_connectome)[1]
+    conn_out_ext = os.path.splitext(out_connectome_filename)[1]
     if symmetric:
         conn_sym = conn.T + conn
         np.fill_diagonal(conn_sym,np.diag(conn))
         if conn_out_ext=='.csv':
-            np.savetxt(output_connectome, conn_sym, delimiter=",")
+            np.savetxt(out_connectome_filename, conn_sym, delimiter=",")
         else:
-            np.save(output_connectome, conn_sym, allow_pickle=False)
+            np.save(out_connectome_filename, conn_sym, allow_pickle=False)
     else:
         if conn_out_ext=='.csv':
-            np.savetxt(output_connectome, conn, delimiter=",")
+            np.savetxt(out_connectome_filename, conn, delimiter=",")
         else:
-            np.save(output_connectome, conn, allow_pickle=False)
+            np.save(out_connectome_filename, conn, allow_pickle=False)
 
-    logger.subinfo( f'Output connectome: "{output_connectome}"', indent_char='*', indent_lvl=1)
+    logger.subinfo( f'Output connectome: "{out_connectome_filename}"', indent_char='*', indent_lvl=1)
     t1 = time()
     logger.info( f'[ {format_time(t1 - t0)} ]' )
